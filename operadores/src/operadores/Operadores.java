ca2d7cbbae95839dfdb31760aebe16982293c309
Guilherme <3guicontact0@gmail.com>
2025-09-27 14:51:16 -0300

    aulas em javs (dnv)

---
+++ operadores/src/operadores/Operadores.java
@@ -0,0 +1,14 @@
+
+package operadores;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        int n1 = 6;
+        int n2 = 6;
+        int soma = n1 + n2;
+        float resu = soma / 2;
+        System.out.println(resu);
+    }
+    
+}
